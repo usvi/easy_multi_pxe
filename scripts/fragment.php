630aa2722876ab794a7fd00022832a8ba4e21561
Janne Paalijarvi <jpaalija@gmail.com>
2024-09-18 13:53:27 +0300

    Preliminary support of printing fragment data

--- scripts/fragment.php
+++ scripts/fragment.php
@@ -2,9 +2,12 @@
 require_once('include_conf.php');
 require_once('include_args.php');
 
+header("Content-Type: text/plain");
+
 $fragment_base_path = $assets_prefix_dir . '/' . $os_family . '/' . $os_version . '/' . $os_arch . '/' . $os_id;
 $fragment_file_path = $fragment_base_path . '.' . $os_arch . '-' . $os_method . '.ipxe';
 
+$fragment_core_data = file_get_contents($fragment_file_path);
 
-print($fragment_file_path);
+print($fragment_core_data);
 ?>
