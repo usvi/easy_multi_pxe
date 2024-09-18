d497963de7afba447e1b249bf66e0df33fe81045
Janne Paalijarvi <jpaalija@gmail.com>
2024-09-18 15:00:21 +0300

    Preseed fragment serves preseed url for debian

--- scripts/fragment.php
+++ scripts/fragment.php
@@ -1,12 +1,10 @@
 <?php
-phpinfo();
-/*
+
 require_once('include_conf.php');
 require_once('include_args.php');
 
 header("Content-Type: text/plain");
 
-print("MORO\n");
 $fragment_file_path =
                     $assets_prefix_dir . '/' . $os_family . '/' . $os_version . '/' . $os_arch . '/' . $os_id .
                     $fragment_base_path . '.' . $os_arch . '-' . $os_method . '.ipxe';
@@ -14,12 +12,15 @@ $fragment_base_url =
                    $webserver_root_url . '/' . $os_family . '/' . $os_version . '/' . $os_arch . '/' . $os_id;
 
 print("set http_base $fragment_base_url\n");
+
 if ($os_family == 'debian')
 {
-    ;
-    }
+    $preseed_url = $webserver_root_url . '/preseed.php&method=' . $os_method . "&family=" .
+                 $os_family . '&version=' . $os_version . '&arch=' . $os_arch . '&id=' . $os_id;
+    print("set preseed_url $preseed_url\n");
+}
 $fragment_core_data = file_get_contents($fragment_file_path);
 
 print($fragment_core_data);
-*/
+
 ?>
