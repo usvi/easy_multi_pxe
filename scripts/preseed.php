8c4e02bc0aef73aa6f87025c4ab014070b902410
Janne Paalijarvi <jpaalija@gmail.com>
2024-09-18 16:00:17 +0300

    Preseeder php working

--- scripts/preseed.php
+++ scripts/preseed.php
@@ -4,10 +4,15 @@ require_once('include_args.php');
 
 header("Content-Type: text/plain");
 
-#$fragment_base_path = $assets_prefix_dir . '/' . $os_family . '/' . $os_version . '/' . $os_arch . '/' . $os_id;
-#$fragment_file_path = $fragment_base_path . '.' . $os_arch . '-' . $os_method . '.ipxe';
+if ($os_family == 'debian')
+{
+    print("#_preseed_V1\n");
+    print("d-i mirror/country string manual\n");
+    print("d-i mirror/protocol string $webserver_protocol\n");
+    print("d-i mirror/http/hostname string $webserver_ip\n");
+    // This magic unpacked is a bit dodgy but lets leave it for now
+    print("d-i mirror/http/directory string /$webserver_path_prefix/$os_family/$os_version/$os_arch/$os_id/unpacked\n");
+    print("d-i debian-installer/allow_unauthenticated boolean true\n");
+}
 
-#$fragment_core_data = file_get_contents($fragment_file_path);
-
-#print($fragment_core_data);
 ?>
