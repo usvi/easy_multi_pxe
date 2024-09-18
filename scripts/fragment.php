38471145e535c48e0831ba2b2de821b0a202dc88
Janne Paalijarvi <jpaalija@gmail.com>
2024-09-18 13:27:30 +0300

    Dynamic printing of ipxe fragment works

--- scripts/fragment.php
+++ scripts/fragment.php
@@ -1,5 +1,10 @@
 <?php
-//require_once('include_conf.php');
+require_once('include_conf.php');
+require_once('include_args.php');
 
-phpinfo();
+$fragment_base_path = $assets_prefix_dir . '/' . $os_family . '/' . $os_version . '/' . $os_arch . '/' . $os_id;
+$fragment_file_path = $fragment_base_path . '.' . $os_arch . '-' . $os_method . '.ipxe';
+
+
+print($fragment_file_path);
 ?>
