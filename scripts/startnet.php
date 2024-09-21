9b5bee02c6a70b688d92dc2f69d398ed56f5f2b5
Janne Paalijarvi <jpaalija@gmail.com>
2024-09-21 13:51:56 +0300

    Dynamic startnet.php works with drivers

--- scripts/startnet.php
+++ scripts/startnet.php
@@ -6,7 +6,8 @@ header("Content-Type: text/dos");
 
 // Assuming this is always windows
 
-print("wpeinit\n");
+
+
 $os_cifs_path =
               '\\\\' . $main_conf_db['CIFS_SERVER_IP'] . '\\' . $arg_os_family . '\\' .
               $arg_os_arch . '\\' . $arg_os_id;
@@ -26,6 +27,28 @@ if (strlen($conf_cifs_passwd) > 0)
     $os_cifs_auth_string .= " $conf_cifs_passwd";
 }
 
+print("echo Loading extra drivers...\n");
+
+if (($conf_drivers_base_dir != "") && (is_dir($conf_drivers_base_dir)))
+{
+    $os_drivers_dir = $conf_drivers_base_dir . '/' . $arg_os_family . '/' . $arg_os_version . '/' . $arg_os_arch;
+
+    if (is_dir($os_drivers_dir))
+    {
+        $driver_file_iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($os_drivers_dir));
+
+        foreach ($driver_file_iterator as $driver_file)
+        {
+            if ($driver_file->getExtension() == "inf")
+            {
+                print("drvload " . $driver_file->getFilename() . "\n");
+            }
+        }
+    }
+}
+
+
+print("wpeinit\n");
 print("net use j: $os_cifs_path $os_cifs_auth_string\n");
 print("j:\\setup.exe\n");
 print("pause\n");
