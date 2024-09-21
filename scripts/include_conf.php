4aa4eb1de8418cc6d6c870ffd38340e58449c8eb
Janne Paalijarvi <jpaalija@gmail.com>
2024-09-21 12:10:01 +0300

    ALl config files parsed, helpers removed

--- scripts/include_conf.php
+++ scripts/include_conf.php
@@ -19,16 +19,52 @@ while(($main_conf_file !== false) && !feof($main_conf_file))
 }
 fclose($main_conf_file);
 
+$conf_webserver_protocol = '';
+$conf_webserver_ip = '';
+$conf_webserver_assets_path_prefix = '';
+$conf_drivers_base_dir = '';
+$conf_webserver_drivers_path_prefix = '';
+$conf_cifs_server_ip = '';
+$conf_cifs_share_name = '';
+$conf_cifs_user = '';
+$conf_cifs_passwd = '';
 
-# Helper variables
-$webserver_protocol = $main_conf_db['WEBSERVER_PROTOCOL'];
-$webserver_ip = $main_conf_db['WEBSERVER_IP'];
-$webserver_assets_path_prefix = $main_conf_db['WEBSERVER_ASSETS_PATH_PREFIX'];
-$webserver_drivers_path_prefix = $main_conf_db['WEBSERVER_DRIVERS_PATH_PREFIX'];
-$drivers_base_dir = $main_conf_db['DRIVERS_BASE_DIR'];
-    
-$webserver_assets_root_url = $webserver_protocol . "://" . $webserver_ip . "/" . $webserver_assets_path_prefix;
-$webserver_drivers_root_url = $webserver_protocol . "://" . $webserver_ip . "/" . $webserver_drivers_path_prefix;
+if (array_key_exists('WEBSERVER_PROTOCOL', $main_conf_db))
+{
+    $conf_webserver_protocol =  $main_conf_db['WEBSERVER_PROTOCOL'];
+}
+if (array_key_exists('WEBSERVER_IP', $main_conf_db))
+{
+    $conf_webserver_ip =  $main_conf_db['WEBSERVER_IP'];
+}
+if (array_key_exists('WEBSERVER_ASSETS_PATH_PREFIX', $main_conf_db))
+{
+    $conf_webserver_assets_path_prefix =  $main_conf_db['WEBSERVER_ASSETS_PATH_PREFIX'];
+}
+if (array_key_exists('DRIVERS_BASE_DIR', $main_conf_db))
+{
+    $conf_drivers_base_dir =  $main_conf_db['DRIVERS_BASE_DIR'];
+}
+if (array_key_exists('WEBSERVER_DRIVERS_PATH_PREFIX', $main_conf_db))
+{
+    $conf_webserver_drivers_path_prefix =  $main_conf_db['WEBSERVER_DRIVERS_PATH_PREFIX'];
+}
+if (array_key_exists('CIFS_SERVER_IP', $main_conf_db))
+{
+    $conf_cifs_server_ip =  $main_conf_db['CIFS_SERVER_IP'];
+}
+if (array_key_exists('CIFS_SHARE_NAME', $main_conf_db))
+{
+    $conf_cifs_share_name =  $main_conf_db['CIFS_SHARE_NAME'];
+}
+if (array_key_exists('CIFS_USER', $main_conf_db))
+{
+    $conf_cifs_passwd =  $main_conf_db['CIFS_USER'];
+}
+if (array_key_exists('CIFS_PASSWD', $main_conf_db))
+{
+    $conf_cifs_passwd =  $main_conf_db['CIFS_PASSWD'];
+}
 
 
 ?>
