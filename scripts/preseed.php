22b5e4047799f1fa6aea7e6512fd82b63c7fa134
Janne Paalijarvi <jpaalija@gmail.com>
2024-09-21 16:56:17 +0300

    Arch variable was misnamed, fixed

--- scripts/preseed.php
+++ scripts/preseed.php
@@ -11,7 +11,7 @@ if ($arg_os_family == 'debian')
     print("d-i mirror/protocol string $conf_webserver_protocol\n");
     print("d-i mirror/http/hostname string $conf_webserver_ip\n");
     // This magic unpacked is a bit dodgy but lets leave it for now
-    print("d-i mirror/http/directory string /$conf_webserver_assets_path_prefix/$arg_os_family/$arg_os_version/$os_arch/$arg_os_id/unpacked\n");
+    print("d-i mirror/http/directory string /$conf_webserver_assets_path_prefix/$arg_os_family/$arg_os_version/$arg_os_arch/$arg_os_id/unpacked\n");
     print("d-i debian-installer/allow_unauthenticated boolean true\n");
 }
 
