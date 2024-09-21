c9089ebdbd241b83b28e7dad620b2f2472e72c53
Janne Paalijarvi <jpaalija@gmail.com>
2024-09-21 14:39:04 +0300

    Embarrassing missing echo fixed.

--- scripts/startnet.php
+++ scripts/startnet.php
@@ -51,7 +51,7 @@ if (($conf_drivers_base_dir != "") && (is_dir($conf_drivers_base_dir)))
 print("wpeinit\n");
 print("net use j: $os_cifs_path$os_cifs_auth_string\n");
 print("j:\\setup.exe /noreboot\n");
-print("Press any key to reboot\n");
+print("echo Press any key to reboot\n");
 print("pause\n");
 print("exit\n");
 
